222550a874f427cc36019f24097b13fb7ecce853
JKurzer <jake.kurzer@gmail.com>
2024-05-08 19:54:24 -0700

    Update ThistleTreeManager.h

--- Source/ThistleRuntime/Public/ThistleTreeManager.h
+++ Source/ThistleRuntime/Public/ThistleTreeManager.h
@@ -14,13 +14,33 @@
  * 
  * Of particular interest:
  * https://dev.epicgames.com/documentation/en-us/unreal-engine/API/Runtime/AIModule/BehaviorTree/Services
+ * https://dev.epicgames.com/community/learning/tutorials/L9vK/unreal-engine-common-issues-with-behavior-trees-and-things-you-should-competely-avoid
  * 
- * It's likely that we don't want thistle to manage trees at all, but instead serve as a service that allows
- * us to abstract the idea of squad dispatch more prettily and to hide some of the awful complexity of networked
- * AI if we need to. That said, the Lyra pawns just used straight Iris, so maybe I'm borrowing trouble and
- * Thistle will just be a place where we put actually normal behavior trees. 
+ * The basic advice is don't do BTrees in C++. We won't be doing our BTrees in C++. But we will be organizing them,
+ * supplementing them, and defining simple aspects of them here, especially around data-driven parts of the design. So why is this class here?
+ * 
+ * Because I need a place to record my thoughts on IF we are going to have a tree manager, and because I think that we'll want
+ * access to some of the member functions some of the time.
+
+ * Outcomes:
+ * 
+ * 1) This is a facade
+ * This just exposes and owns helper functions, and just simplifies loading and paths. This is the likeliest outcome.
  *
- * If either happens, this will probably merge with the ThistleDispatch, and may end up encapsulated even if not merged.
+ * 2) This is a shim
+ * Maybe we don't want thistle to do more than select trees. This lets us hide some of the awful complexity of networked
+ * AI if we need to. That said, the Lyra pawns just used straight Iris, so maybe I'm borrowing trouble and
+ * Thistle will just be a place where we put actually totally normal behavior trees. In that case, this shim is short-lived
+ *  
+ * 3) blackboard manager, not behavior tree manager
+ * It's likely that we'll be able to do almost everything we want by combining data-binding tools like DDG and config files
+ * with dynamic management and composition of blackboards. In this model, we keep our tree manager around but it really just
+ * associates trees with blackboards and really owns blackboards.
+ * 
+ * 4) I'm wrong, and I'm actually smart.
+ * Kinda hoping this one doesn't come to pass. If we have to use Mass, we'll need a way to hide the fact that Mass controls our
+ * units most of the time. Then this becomes a way to abstract swapping into and out of archetype-linked behavior trees for finegrained
+ * ai near the players. I always want to minimize our dependency on Mass, cause I'm nervous about the state of it.
  */
 UCLASS()
 class THISTLERUNTIME_API UThistleTreeManager : public UBehaviorTreeManager
